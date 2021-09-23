cd2cb92419adde5b8acfc83d49591401e1b5686d
Tomasz Pająk <tomasz@pajak.name>
2021-09-23 08:15:37 +0200

    solution

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -18,6 +18,12 @@ package com.example.android.trackmysleepquality
 
 import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
+import androidx.lifecycle.Transformations
+import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
 
 /**
@@ -27,6 +33,7 @@ import java.text.SimpleDateFormat
 /**
  * Returns a string representing the numeric quality rating.
  */
+
 fun convertNumericQualityToString(quality: Int, resources: Resources): String {
     var qualityString = resources.getString(R.string.three_ok)
     when (quality) {
@@ -53,7 +60,7 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
 @SuppressLint("SimpleDateFormat")
 fun convertLongToDateString(systemTime: Long): String {
     return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
-            .format(systemTime).toString()
+        .format(systemTime).toString()
 }
 
 /**
@@ -72,32 +79,33 @@ fun convertLongToDateString(systemTime: Long): String {
 
 // TODO (11) Uncomment the code.
 
-//fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
-//    val sb = StringBuilder()
-//    sb.apply {
-//        append(resources.getString(R.string.title))
-//        nights.forEach {
-//            append("<br>")
-//            append(resources.getString(R.string.start_time))
-//            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
-//            if (it.endTimeMilli != it.startTimeMilli) {
-//                append(resources.getString(R.string.end_time))
-//                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
-//                append(resources.getString(R.string.quality))
-//                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
-//                append(resources.getString(R.string.hours_slept))
-//                // Hours
-//                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
-//                // Minutes
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
-//                // Seconds
-//                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-//            }
-//        }
-//    }
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-//    } else {
-//        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
-//    }
-//}
+fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.title))
+        nights.forEach {
+            append("<br>")
+            append(resources.getString(R.string.start_time))
+            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+            if (it.endTimeMilli != it.startTimeMilli) {
+                append(resources.getString(R.string.end_time))
+                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+                append(resources.getString(R.string.quality))
+                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+                append(resources.getString(R.string.hours_slept))
+                // Hours
+                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+                // Minutes
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+                // Seconds
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+            }
+        }
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+}
+
